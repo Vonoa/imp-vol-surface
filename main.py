8654c86a07872923f80d7bbb854a74d201d4dd40
Aled von Oppell <alvonopp@btinternet.com>
2026-07-14 20:19:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import threading
+import time
+import pandas as pd
+import numpy as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Button
